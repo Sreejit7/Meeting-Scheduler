332812fd824148da7002fe4875477636919ac3ff
Sreejit De <sreejitde7@gmail.com>
2021-06-27 20:59:11 +0530

    Added views (#1)

--- src/components/Scheduler/index.js
+++ src/components/Scheduler/index.js
@@ -1,10 +1,20 @@
-import { Inject, Day, Week, WorkWeek, Month, Agenda, ScheduleComponent } from '@syncfusion/ej2-react-schedule';
+import { Inject, Day, Week, WorkWeek, Month, Agenda, ScheduleComponent, ViewsDirective, ViewDirective } from '@syncfusion/ej2-react-schedule';
+import { ButtonComponent } from '@syncfusion/ej2-react-buttons'
 
 const Scheduler = () => {
+  
   return(
+    <>
     <ScheduleComponent currentView="Month">
+      <ViewsDirective>
+          <ViewDirective option='Day'/>
+          <ViewDirective option='Week'/>
+          <ViewDirective option='WorkWeek'/>
+          <ViewDirective option='Month'/>
+      </ViewsDirective>
       <Inject services = {[Day, Week, WorkWeek, Month, Agenda]} />
     </ScheduleComponent>
+    </>
   );
 }
 
